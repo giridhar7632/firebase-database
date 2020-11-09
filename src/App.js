09516bc097715d1e1f585c1fce1d8a81ee82a217
giridhar7632 <giridhar.talla2002@gmail.com>
2020-11-09 08:19:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import fireBase from './Firebase'
 
+/*function useInput(initialValue){
+   const [value,setValue] = useState(initialValue);
+
+    function handleChange(e){
+        setValue(e.target.value);
+    }
+
+   return [value,handleChange];
+}
+*/
 function App() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+
+  function handleName(e) {
+    setName(e.target.value)
+  }
+  function handleEmail(e) {
+    setEmail(e.target.value)
+  }
+  function handleSubmit(e) {
+    console.log(name, email)
+    let messageRef = fireBase.database().ref('messages')
+    messageRef.push([name, email])
+    setName("")
+    setEmail("")
+    e.preventDefault()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <form>
+      <h1> Form </h1>
+        <label type="Name:">
+          <input type="text" value={name} onChange={handleName} placeholder="Your Name"/>
+        </label>
+        <label type="Email:">
+          <input type="email" value={email} onChange={handleEmail} placeholder="Your Email"/>
+        </label>
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
   );
 }
 
